8bdeef0720560c7e6f81d90a37379d4c1d41b1f0
markuserkmann <markus.erkmann@voco.ee>
2024-11-08 11:24:09 +0200

    Finished the poker exercise with max points on solution and style

--- OOP Excercises/poker.py
+++ OOP Excercises/poker.py
@@ -19,7 +19,6 @@ class Card:
         """
         return f"{self.value} of {self.suit}"
 
-
 class Hand:
     """The hand in a poker game."""
 
@@ -31,14 +30,7 @@ class Hand:
         self.cards = [] 
 
     def can_add_card(self, card: Card) -> bool:
-        """
-        Check for card validity.
-
-        Can only add card if:
-        - A card with the same suit and value is already not being held.
-        - The player is holding less than five cards.
-        - The card has both a valid value and a valid suit.
-        """
+        """ Check if the card is valid and can be added to the hand. """
         if card.value not in self.values or card.suit not in self.suits:
             return False
         for c in self.cards:
@@ -49,163 +41,108 @@ class Hand:
         return True
 
     def add_card(self, card: Card):
-        """
-        Add a card to hand.
-
-        Before adding a card, you would have to check if it can be added.
-        """
+        """Add a valid card to the hand."""
         if self.can_add_card(card):
             self.cards.append(card)
         else:
             print(f"Cannot add {card}. Invalid card or hand is full.")
 
     def can_remove_card(self, card: Card):
-        """
-        Check if a card can be removed from hand.
-
-        The only consideration should be that the card is already being held.
-        """
+        """Check if a card can be removed from the hand."""
         return card in self.cards
 
     def remove_card(self, card: Card):
-        """
-        Remove a card from hand.
-
-        Before removing the card, you would have to check if it can be removed.
-        """
+        """Remove a card from the hand."""
         if self.can_remove_card(card):
             self.cards.remove(card)
 
     def get_cards(self):
-        """Return a list of cards as objects."""
+        """Return a list of cards in the hand."""
         return self.cards
 
     def is_straight(self):
-        """
-        Determine if the hand is a straight.
-        A straight consists of 5 consecutive cards in order of value.
-        Ace is treated as the highest value (above the King).
-        """
+        """ Check if the hand is a straight. """
         values = [card.value for card in self.cards]
-        
         values.sort(key=lambda v: self.values.index(v))
-        
         for i in range(1, len(values)):
             if self.values.index(values[i]) != self.values.index(values[i - 1]) + 1:
                 return False
-
         return True
 
     def is_flush(self):
-        """
-        Determine if the hand is a flush.
-        A flush consists of 5 cards all of the same suit.
-        """
+        """ Check if the hand is a flush. """
         suits = [card.suit for card in self.cards]
         return len(set(suits)) == 1 
 
     def is_straight_flush(self):
-        """
-        Determine if the hand is a straight flush.
-
-        Such a hand is both straight and flush at the same time.
-
-        """
-        raise NotImplementedError
+        """ Check if the hand is a straight flush. """
+        return self.is_straight() and self.is_flush()
 
     def is_full_house(self):
-        """
-        Determine if the hand is a full house.
-
-        A house will have three cards of one value, and two cards of a second value.
-        For example:
-        2 2 2 6 6
-        K J K J K
-        """
-        raise NotImplementedError
+        """ Check if the hand is a full house. """
+        values = [card.value for card in self.cards]
+        value_counts = {value: values.count(value) for value in values}
+        return sorted(value_counts.values()) == [2, 3]
 
     def is_four_of_a_kind(self):
-        """
-        Determine if there are four cards of the same value in hand.
-
-        For example:
-        2 2 K 2 2
-        9 4 4 4 4
-
-        """
-        raise NotImplementedError
+        """ Check if the hand is four of a kind. """
+        values = [card.value for card in self.cards]
+        value_counts = {value: values.count(value) for value in values}
+        return 4 in value_counts.values()
 
     def is_three_of_a_kind(self):
-        """
-        Determine if there are three cards of the same value in hand.
-
-        For Example:
-        Q 4 Q Q 7
-        5 5 1 5 2
-
-        """
-        raise NotImplementedError
+        """ Check if the hand is three of a kind. """
+        values = [card.value for card in self.cards]
+        value_counts = {value: values.count(value) for value in values}
+        return 3 in value_counts.values()
 
     def is_pair(self):
-        """
-        Determine if there are two kinds of the same value in hand.
-
-        For example:
-        5 A 2 K A
-        8 7 6 6 5
-
-        """
-        raise NotImplementedError
+        """ Check if the hand is a pair. """
+        values = [card.value for card in self.cards]
+        value_counts = {value: values.count(value) for value in values}
+        return 2 in value_counts.values()
 
     def get_hand_type(self):
-        """
-        Return a string representation of the hand.
-
-        Return None (or nothing), if there are less than five cards in hand.
-
-        "straight flush" - Both a straight and a flush
-        "flush" - The cards are all of the same suit
-        "straight" - The cards can be ordered
-        "full house" - Three cards are of the same value while the other two also share a value.
-        "four of a kind" - Four cards are of the same value
-        "three of a kind" - Three cards are of the same value
-        "pair" - Two cards are of the same value
-        "high card" - None of the above
-
-        """
-        raise NotImplementedError
+        """ Return the type of the hand. """
+        if len(self.cards) < 5:
+            return None
+        if self.is_straight_flush():
+            return "straight flush"
+        elif self.is_flush():
+            return "flush"
+        elif self.is_straight():
+            return "straight"
+        elif self.is_full_house():
+            return "full house"
+        elif self.is_four_of_a_kind():
+            return "four of a kind"
+        elif self.is_three_of_a_kind():
+            return "three of a kind"
+        elif self.is_pair():
+            return "pair"
+        else:
+            return "high card"
 
     def __repr__(self):
-        """
-        Return a string representation of the hand.
-
-        I got a {type} with cards: {card list}
-        I got a straight with cards: 2 of diamonds, 4 of spades, 5 of clubs, 3 of diamonds, 6 of hearts
-
-        If a hand type cannot be yet determined, return a list of cards as so:
-
-        I'm holding {cards}
-        I'm holding 2 of diamonds, 4 of spades.
-
-        Order of the cards is not important.
-        """
-        raise NotImplementedError
-
+        """ Return a string representation of the hand. """
+        hand_type = self.get_hand_type()
+        if hand_type:
+            return f"I got a {hand_type} with cards: {', '.join(str(card) for card in self.cards)}"
+        else:
+            return f"I'm holding {', '.join(str(card) for card in self.cards)}"
 
 if __name__ == "__main__":
     hand = Hand()
     cards = [Card("2", "diamonds"), Card("4", "spades"), Card("5", "clubs"), Card("3", "diamonds"), Card("6", "hearts")]
     [hand.add_card(card) for card in cards]
-    assert hand.get_hand_type() == "straight"
+    print(hand.get_hand_type())
 
     hand = Hand()
-    cards = [Card("10", "diamonds"), Card("2", "diamonds"), Card("A", "diamonds"), Card("6", "diamonds"),
-             Card("9", "diamonds")]
+    cards = [Card("10", "diamonds"), Card("2", "diamonds"), Card("A", "diamonds"), Card("6", "diamonds"), Card("9", "diamonds")]
     [hand.add_card(card) for card in cards]
-    assert hand.get_hand_type() == "flush"
+    print(hand.get_hand_type())
 
     hand = Hand()
-    cards = [Card("A", "hearts"), Card("A", "clubs"), Card("A", "spades"), Card("A", "diamonds"),
-             Card("9", "diamonds")]
+    cards = [Card("A", "hearts"), Card("A", "clubs"), Card("A", "spades"), Card("A", "diamonds"), Card("9", "diamonds")]
     [hand.add_card(card) for card in cards]
-    assert hand.get_hand_type() == "four of a kind"
+    print(hand.get_hand_type()) 
